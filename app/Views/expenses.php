ec873d16c78bc58315c99962cd3e21a738cafa1f
Domurad Artur <ADomurad@hemmersbach.com>
2021-10-15 16:56:33 +0200

    View update

--- app/Views/expenses.php
+++ app/Views/expenses.php
@@ -1,8 +1,9 @@
-
+    
+    
     <section class="top-bar-container">
         <div class="brand-box">
-            <img class="brand-logo-img" src="assets/img/logo.png" alt="logo">
-            <div class="brand-logo-text">Budget</div> <!-- optional -->
+            <img class="brand-logo-img" src="assets/img/Budget_logo.svg" alt="logo">
+            <!-- <div class="brand-logo-text">Budget</div> --> <!-- optional -->
         </div>
         <div class="user-box">
             <div class="balance-box">
@@ -14,30 +15,80 @@
         </div>
     </section>
 
-    <section class="left-bar-container">
-        <div class="left-bar">
-            <div class="top-menu">
-                <a class="item-link item" href="#">
-                    <img class="item-icon" src="assets/img/logo.png" alt="item.icon">
-                    <p class="item-text">iteem_name</p> 
-                </a>
-                <a class="item-link item" href="#">
-                    <img class="item-icon" src="assets/img/logo.png" alt="item.icon">
-                    <p class="item-text">iteem_name</p> 
-                </a>
-                <a class="item-link item" href="#">
-                    <img class="item-icon" src="assets/img/logo.png" alt="item.icon">
-                    <p class="item-text">iteem_name</p>
-                </a>
+    <section class="container">
+        <div class="left-bar-container">
+            <div class="left-bar">
+                <div class="top-menu">
+                    <div class="menu-group">
+                        <div class="group-label">
+                            <img src="assets/img/tables.svg" alt="" class="label-img">
+                            <p class="label-text">Tables</p>
+                        </div> <!-- can colapse items div -->
+                        <div class="items">
+                            <a class="item-link item" href="#">
+                                <img class="item-icon" src="assets/img/Expenses.svg" alt="item.icon">
+                                <p class="item-text">Expenses</p> 
+                            </a>
+                            <a class="item-link item" href="#">
+                                <img class="item-icon" src="assets/img/Incomes.svg" alt="item.icon">
+                                <p class="item-text">Incomes</p> 
+                            </a>
+                            <a class="item-link item" href="#">
+                                <img class="item-icon" src="assets/img/logo.png" alt="item.icon">
+                                <p class="item-text">iteem_name</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom-menu">
+                    <a class="item-link item" href="#">
+                        <img class="item-icon" src="assets/img/logo.png" alt="item.icon">
+                    </a>
+        
+                    <a class="item-link item" href="#">
+                        <img class="item-icon" src="assets/img/logout.svg" alt="item.icon">
+                    </a>
+                </div>
             </div>
-            <div class="bottom-menu">
-                <a class="item-link item" href="#">
-                    <img class="item-icon" src="assets/img/logo.png" alt="item.icon">
-                </a>
-    
-                <a class="item-link item" href="#">
-                    <img class="item-icon" src="assets/img/logo.png" alt="item.icon">
-                </a>
+        </div>
+        <div class="content-container">
+            <div class="header-box">
+                <div class="header">Examle Header</div>
+                <div class="sub-header">Lorem ipsum dolor sit, amet consectetur adipisicing</div>
+            </div>
+            <div class="table-box">
+                <div class="upper-table">
+                    <div class="table-filters">
+                        <div class="filter">
+                            <p class="filter-text">By dates</p>
+                            <div class="inputs-row">
+                                <label for="">From: </label>
+                                <input type="date" name="" id=""> 
+                                <label for="">To: </label>
+                                <input type="date" name="" id="">
+                            </div>
+                        </div>
+                        <hr class="flters-separator"></hr>
+                    </div>
+                    
+                    <div class="table-buttons">
+                        <div class="button">
+                            <img class="button-icon" src="" alt=""></img>
+                            <p class="button-text">Edit</p>
+                        </div>
+                        <div class="button">
+                            <img class="button-icon" src="" alt=""></img>
+                            <p class="button-text">Add</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="table">
+                    <!-- datatable here -->
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat ab asperiores dolores? Voluptatibus, autem? Perferendis eaque, dolores reiciendis magni molestias hic non repudiandae ea facere voluptatibus, saepe cum voluptate odit.
+                    Eveniet eligendi, blanditiis quaerat nemo rem dolorum aliquam tenetur excepturi, ad repudiandae corporis accusamus assumenda veniam reprehenderit saepe numquam commodi at eius quas eos quae praesentium neque error? Vitae, dolorem?
+                    
+                    <p></p>
+                </div>
             </div>
         </div>
     </section>
